2135979d033eb646bbadd329dbdfb00b61d552ed
akashmngkmit <akashnandan@gkmit.co>
2025-11-14 01:31:56 +0530

    chore: add vitest config

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app = express();
+export const app = express();
 
 // Middleware
 app.use(cors());
@@ -20,3 +20,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port http://localhost:${PORT}`)
 );
+
+
