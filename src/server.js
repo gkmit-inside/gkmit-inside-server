bea078a4baae8d7e0d8fb475710400cbee7facdd
akashmngkmit <akashnandan@gkmit.co>
2025-11-14 00:56:14 +0530

    chore: add prettier config and refine folder structure

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -11,10 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 // Basic route
-app.get("/", (req, res) => {
-  res.send("Gkmit Server is running...");
+app.get('/', (req, res) => {
+  res.send('Gkmit Server is running...');
 });
 
 // Server start
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
