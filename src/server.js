2b3b6f0618d6444f0c9fb5a707c7cd702264a559
akashmngkmit <akashnandan@gkmit.co>
2025-11-14 23:26:14 +0530

    chore(deps): add mongodb config

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import { connectDB } from './config/db.js';
+
 dotenv.config();
 
 export const app = express();
@@ -17,8 +19,7 @@ app.get('/', (req, res) => {
 
 // Server start
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port http://localhost:${PORT}`)
-);
-
-
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
