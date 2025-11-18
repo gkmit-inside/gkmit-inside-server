5e1595b9c79da4c72560425c85162023a1055a37
akashmngkmit <akashnandan@gkmit.co>
2025-11-18 16:01:55 +0530

    feat: implement test cases

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import './config/env.js';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { authRouter } from './routes/auth.routes.js';
+import { postRouter } from './routes/post.routes.js';
+import { adminRouter } from './routes/admin.routes.js';
+
+import { globalErrorHandler } from './middlewares/errorHandler.js'; 
+
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // Your frontend URL
+  credentials: true,
+  optionsSuccessStatus: 200         
+};
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+app.use(express.json()); 
+
+// --- API Routes ---
+app.get('/', (req, res) => {
+    res.send('GKMIT Server is running...');
+});
+
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
+app.use('/api/admin', adminRouter);
+
+app.use(globalErrorHandler);
+
+export { app }; 
